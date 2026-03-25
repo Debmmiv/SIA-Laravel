df7961b3a84ecc8977b342bc27c30148681a8032
John Dave Cinco <cinco.johndave87@gmail.com>
2026-03-25 11:16:50 +0800

    Initial Commit

---
+++ resources/views/components/application-logo.blade.php
@@ -0,0 +1,1 @@
+<img src="{{ asset('images/Application-logo.png') }}" alt="System Logo" {{ $attributes }}>
---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductsController;
+use App\Http\Controllers\OrdersController;
+use App\Http\Controllers\PaymentsController;
+use App\Http\Controllers\HistoryController;
+use App\Http\Controllers\AboutController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/customer', [CustomerController::class, 'index'])->name('customer.index');
+    Route::get('/product', [ProductsController::class, 'index'])->name('product.index');
+    Route::get('/Order', [OrdersController::class, 'index'])->name('Order.index');
+    Route::get('/payment', [PaymentsController::class, 'index'])->name('payment.index');
+    Route::get('/history-nav', [HistoryController::class, 'index'])->name('history-nav.index');
+    Route::get('/about', [AboutController::class, 'index'])->name('about.index');
+}); 
+
+require __DIR__.'/auth.php';
